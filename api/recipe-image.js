b371029efc0a79b02793b28df8930eef3820ff9a
Nkp <m24c58s7nk@privaterelay.appleid.com>
2026-09-25 15:13:45 -0500

    Allow Tablely frontend to call recipe image search

--- api/recipe-image.js
+++ api/recipe-image.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+export default async function handler(req, res) {\n  res.setHeader("Access-Control-Allow-Origin", "*");\n  res.setHeader("Access-Control-Allow-Headers", "Content-Type");\n  if (req.method === "OPTIONS") return res.status(204).end();
   if (req.method !== "POST") return res.status(405).json({ error: "Method not allowed" });
 
   try {
