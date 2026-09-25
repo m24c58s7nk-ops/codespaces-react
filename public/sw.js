e7188bd28b02d7f3ee7099731417bd9b112d3c94
Nkp <m24c58s7nk@privaterelay.appleid.com>
2026-09-25 10:23:54 -0500

    Fix PWA service worker for GitHub Pages

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,28 @@
-const CACHE="tablely-v1";
-self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(["/","/index.html"]))));
+const BASE = self.location.pathname.replace(/\/sw\.js$/, "/");
+const CACHE = "tablely-v2";
+const APP_SHELL = [BASE, BASE + "index.html"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(() => {});
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
